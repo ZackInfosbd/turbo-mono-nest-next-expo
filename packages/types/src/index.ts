954c4c514647a95599b475b956bf79064c85db9d
Zakaria <68170594+ZackInfosbd@users.noreply.github.com>
2024-11-24 23:13:15 +0100

    fix: shared types & dtos

---
+++ packages/types/src/index.ts
@@ -0,0 +1,4 @@
+export * from './dtos/auth/create-auth.dto';
+export * from './dtos/auth/update-auth.dto';
+export * from './dtos/users/create-user.dto';
+export * from './dtos/users/update-user.dto';
