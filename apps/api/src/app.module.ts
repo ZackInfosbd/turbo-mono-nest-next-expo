329a47766c946ab2b36272b1bc8e6c2a026d6174
Zakaria <68170594+ZackInfosbd@users.noreply.github.com>
2024-12-27 18:33:23 +0100

    feat(prisma.schema): prisma & GraphQl
    steup prisma with graphql for api

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,27 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      introspection: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      buildSchemaOptions: {
+        numberScalarMode: 'integer',
+      },
+      fieldResolverEnhancers: ['guards'],
+    }),
+    ConfigModule.forRoot(),
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
