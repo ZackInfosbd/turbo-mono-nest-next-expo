16acfe893dd760c34fe8018f5c84ce36e54f4f20
Zakaria <68170594+ZackInfosbd@users.noreply.github.com>
2024-12-29 22:14:46 +0100

    fix(formcreateitemserver): resolving linting issue

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,5 +1,1 @@
-export * from './dtos/auth/create-auth.dto';
-export * from './dtos/auth/update-auth.dto';
-export * from './dtos/users/create-user.dto';
-export * from './dtos/users/update-user.dto';
 export * from './ui/base-component';
--- packages/ui/src/components/organisms/formCreateItemServer.tsx
+++ packages/ui/src/components/organisms/formCreateItemServer.tsx
@@ -4,8 +4,15 @@ import { Button } from '../atoms/button';
 import { createItem } from '@repo/hooks/actions';
 
 export const FormCreateItemServer = () => {
+  async function handleCreateItem(formData: FormData) {
+    const result = await createItem(formData);
+    if (result) {
+      throw result;
+    }
+  }
+
   return (
-    <form className="flex flex-col gap-2" action={createItem}>
+    <form className="flex flex-col gap-2" action={handleCreateItem}>
       <Label title="Item">
         <Input placeholder="Enter item name" name="name" />
       </Label>
