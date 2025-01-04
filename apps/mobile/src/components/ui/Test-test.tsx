265da3b922256237acba479be96230279fda7583
Zakaria <68170594+ZackInfosbd@users.noreply.github.com>
2025-01-05 00:43:02 +0100

    feat(prisma): prisma shema
    schema mainly for auth&authZ, user, profile, prferences and security like bloacks as (session -
    security logs  management)

--- apps/mobile/src/components/ui/Test-test.tsx
+++
@@ -1,6 +0,0 @@
-import { PropsWithChildren } from 'react';
-import { View } from 'react-native';
-
-export default function Test({ children }: PropsWithChildren) {
-  return <View>{children}</View>;
-}
